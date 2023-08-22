54a40910a9af7d5185ded62b8a37337764cea10b
kauanysc16 <123770473+kauanysc16@users.noreply.github.com>
2023-08-22 13:17:55 -0300

    Add files via upload
    22/08/2023

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener('scroll', function() {
-        const navbar = document.getElementById('navbar');
-        if (window.scrollY > 100) {
-            navbar.classList.add('scroll-nav');
-        } else {
-            navbar.classList.remove('scroll-nav');
-        }
-    });
-});
+    var entrada = document.querySelector(".entrada");
+    setTimeout(function() {
+      entrada.classList.add("animacao");
+    }, 1000);
+  });
+  
